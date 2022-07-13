44eb8964575b92c9b3c7a9934cd2517fe47f246e
Manish patil <99861707+patilmanish07@users.noreply.github.com>
2022-07-14 00:43:26 +0530

    Add files via upload

---
+++ Enum1.cpp
@@ -0,0 +1,41 @@
+//// Program used to demonstrate the concept of Enumeration in C++ & C.
+#include<iostream>
+using namespace std;
+  
+   int main()
+  {
+    //          0   1   2   3   4    5   6
+    enum Days {Sun,Mon,Tue,Wed,Thus,Fri,Sat}; 
+	
+	 enum Days obj;
+	
+	 obj = Tue;
+	
+	 cout<<obj<<"\n";
+	 cout<<sizeof(obj)<<"\n";
+     
+	 enum Gender {Female = 1,Male = 2};
+	 cout<<"Enter your gender\n";
+	 cout<<"1: Female\n";
+	 cout<<"2: Male\n";
+	 
+	 cin>>Input;
+	 
+	  switch(Input)
+	 {
+	    case Female:
+		      cout<<"Tax free limit is 3,00,000\n";
+		      break;
+		   
+		case Male:
+		       cout<<"Tax free limit is 2,50,000\n";
+		       break;
+	  
+        default:
+               cout<<"Invalid Input\n";	
+               break;
+     }			   
+	 
+	     return 0;
+  }
+  
